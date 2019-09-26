56dca2f088b2bae56b6588b2d0e78805d6f0af39
Carlos Smolareck <carlos_smolareck@dell.com>
2019-09-25 21:27:36 -0300

    Criacao projeto netcore

---
+++ projeto-net-core.core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projeto_net_core.core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
